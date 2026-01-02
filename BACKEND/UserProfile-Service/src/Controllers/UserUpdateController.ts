a71ad7de58805bc02cb99c2bf2d20436f9f49e63
Riann121 <145764804+Riann121@users.noreply.github.com>
2026-01-03 04:38:29 +0600

    [UserProfile][ADD]- update data of the user

--- BACKEND/UserProfile-Service/src/Controllers/UserUpdateController.ts
+++ BACKEND/UserProfile-Service/src/Controllers/UserUpdateController.ts
@@ -1,10 +1,41 @@
 import { Request, Response } from "express";
 import { ErrorHandler } from "../Utils/customErrorHandler.js";
+import prisma from "../Config/prisma.js";
 
 const UserUpdateController = async (req: Request, res: Response) => {
     try {
+        //AUTHENTICATED USER PROFILE AND UPDATE DATA
+        const profile = req.body.auth
+        const updatedData = req.body.updateData
+        if(!profile){
+            //HANDLE UNAUTHORIZED ACCESS
+            ErrorHandler('Unauthorized', new Error('No profile data found'), res, 401);
+            return;
+        }
+        if(!updatedData){
+            //HANDLE MISSING UPDATE DATA
+            ErrorHandler('No update data provided', new Error('Update data is required'), res, 400);
+            return;
+        }
+        try {
+            //UPDATE USER DATA IN DATABASE
+            const updatedUser = await prisma.client.update({
+                where: profile.id,
+                data: updatedData
+            })
+            //EXCLUDE PASSHASH FROM RESPONSE
+            const {passhash, ...showUser} = updatedUser
+            res.status(200).json({ message: 'User updated successfully', showUser });
+
+        } catch (error) {
+            // HANDLE UPDATE FAILURE
+            ErrorHandler('User update failed', error as Error, res, 404);
+        }
         
     } catch (error) {
+        //HANDLE GENERAL ERROR
         ErrorHandler('Error updating user', error as Error, res, 500);
     }
 }
+
+export default UserUpdateController;
