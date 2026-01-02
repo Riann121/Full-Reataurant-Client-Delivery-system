2934c61219a474163321d7ccdf8ee74e656e6586
Riann121 <145764804+Riann121@users.noreply.github.com>
2026-01-03 04:40:37 +0600

    [UserProfile][ADD]- Delete User

---
+++ BACKEND/UserProfile-Service/src/Controllers/UserDeleteController.ts
@@ -0,0 +1,37 @@
+import {Request,Response} from 'express';
+import { ErrorHandler } from '../Utils/customErrorHandler.js';
+import prisma from '../Config/prisma.js';
+
+
+//USER DELETE CONTROLLER
+const UserDeleteController = async (req:Request, res:Response) => {
+    try {
+        const clientId = req.params.id;
+        if(req.params.id === undefined){
+            //HANDLE MISSING NUMBER PARAMETER
+            return ErrorHandler('Missing user', new Error('Client Id is required'), res, 400);
+        }
+        try {
+            //DELETE USER BY ID
+            const user = await prisma.client.delete({
+                where:{id: clientId}
+            });
+            if(!user){
+                //HANDLE USER NOT FOUND
+                return ErrorHandler('User not found', new Error('No Client with the provided Id'), res, 404);
+            }
+            if(user){
+                //SEND SUCCESS RESPONSE
+                res.status(204).json({ message: 'User deleted successfully'}); 
+            }
+        } catch (error) {
+            ErrorHandler('Error deleting User try again', error as Error, res, 500);
+        }
+       
+    } catch (error) {
+        //EANDLE GENERAL ERROR
+        ErrorHandler('Error processing request', error as Error, res, 500);
+    }
+};
+
+export default UserDeleteController;
