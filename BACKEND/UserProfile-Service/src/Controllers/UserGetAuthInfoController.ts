0ed5fb59d10673f0e82cfb6bb8ef19f0cf78fc9c
Riann121 <145764804+Riann121@users.noreply.github.com>
2026-03-17 22:40:18 +0600

    [USER-PROFILE-SERVICE][ADD] controller for auth service to grab the user info

---
+++ BACKEND/UserProfile-Service/src/Controllers/UserGetAuthInfoController.ts
@@ -0,0 +1,32 @@
+import {Request,Response} from 'express';
+import { ErrorHandler } from '../Utils/customErrorHandler.js';
+import prisma from '../Config/prisma.js';
+import { SuccessHandler } from '../Utils/customSuccessHandler.js';
+
+//USER GET AUTHENTICATION INFO CONTROLLER
+const UserGetAuthInfoController = async (req:Request, res:Response) => {
+    try {
+        const clientNumber = req.params.number;
+        if(req.params.number === undefined){
+            //HANDLE MISSING NUMBER PARAMETER
+            return ErrorHandler('Missing number', new Error('Phone Number is required'), res, 400);
+        }
+        //FIND USER BY ClientId
+        const user = await prisma.client.findMany({
+            where:{number: clientNumber}
+        });
+        if (!user) {
+            //HANDLE USER NOT FOUND
+            ErrorHandler('User not found', new Error('No Client with the provided number'), res, 404);
+        }
+        else {
+            //SEND USER DATA RESPONSE
+            SuccessHandler(user, res, 200, 'User retrieved successfully');
+        }
+    } catch (error) {
+        //HANDLE GENERAL ERROR
+        ErrorHandler('Error retrieving User', error as Error, res, 500);
+    }
+};
+
+export default UserGetAuthInfoController;
